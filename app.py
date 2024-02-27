9a5b57c65fe1ab130952205f010287dfc289d153
Micke Nordin <hej@mic.ke>
2024-02-27 09:55:42 +0100

    Add support for user info

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
+import yaml
 
 drive_sites = []
-sites = [
-    "antagning", "bth", "chalmers", "du", "esh", "extern", "fhs", "gih", "gu",
-    "hb", "hh", "hhs", "hig", "his", "hj", "hkr", "hv", "irf", "kau", "kb",
-    "ki", "kkh", "kmh", "konstfack", "kth", "kva", "liu", "lnu", "ltu", "lu",
-    "mau", "mdu", "miun", "nordunet", "nrm", "oru", "rkh", "scilifelab", "shh",
-    "sics", "slu", "smhi", "sp", "su", "sunet", "suni", "swamid", "ths", "uhr",
-    "umu", "uniarts", "uu", "vinnova", "vr"
-]
+if not os.path.exists("config.yaml"):
+    raise Exception("config.yaml file not found")
+with open("config.yaml", 'r') as fh:
+    yml = yaml.safe_load(fh)
+    sites = yml["sites"]
+    domain = yml["domain"]
 
-for site in sorted(sites):
-    caption = f"{site}"
-    href = f"https://{caption}.drive.test.sunet.se/"
-    drive_sites.append({"href": href, "caption": caption})
+    for site in sorted(sites):
+        caption = f"{site}"
+        href = f"https://{caption}.{domain}/"
+        drive_sites.append({"href": href, "caption": caption})
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", drive_sites=drive_sites)
+    user_info = {}
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json' and request.method == 'POST'):
+        json = request.get_json()
+        # {
+        #   "user_id": ...
+        #   "displayname": ...
+        #   "timestamp": ...
+        #   "issuer": ...
+        #   "service": ...
+        # }
+        user_info['displayname'] = json['displayname']
+        user_info['domain'] = json['user_id'].split('@')[1]
+        user_info['site'] = json['service'].lstrip('https://').rstrip('/index.php/apps/user_saml/saml/metadata')
+    return render_template("index.html", drive_sites=drive_sites, domain=domain, user_info=user_info)
--- static/drive.js
+++ static/drive.js
@@ -2,16 +2,23 @@ function filter_sites() {
   var text = document.getElementById("search_box").value;
   var list = document.getElementById("drive_sites");
   var items = list.getElementsByTagName("li");
+  var visible = false;
+  var extern_id = 0;
   for (var i = 0; i < items.length; ++i) {
     var item = items[i];
     var item_text = item.textContent || item.innerText;
     item_text = item_text.toLowerCase();
     if (item_text.indexOf(text.toLowerCase()) > -1) {
       item.style.display = "block";
+      visible = true;
     } else {
-      if (item_text != "extern") {
-        item.style.display = "none";
-      }
+      item.style.display = "none";
     }
+    if (item_text.indexOf('extern') > -1) {
+      extern_id = i;
+    }
+  }
+  if (!visible) {
+    document.getElementById(extern_id).style.display = "block";
   }
 }
