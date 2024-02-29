5b38e07e5486374906f03a0b867888068540c2ae
Micke Nordin <kano@sunet.se>
2024-02-29 12:36:34 +0100

    Add support for direct login

--- static/drive.js
+++ static/drive.js
@@ -1,3 +1,20 @@
+function use_direct() {
+  var list = document.getElementById("drive_sites");
+  var items = list.getElementsByTagName("li");
+  var checkbox_state = document.getElementById("direct_login").checked; 
+  var suffix = "index.php/login?direct=1";
+  for (var i = 0; i < items.length; ++i) {
+    link = items[i].getElementsByTagName("a")[0];
+    if (!checkbox_state) {
+      if (link.href.indexOf( suffix ) != -1) {
+        link.href = link.href.substring( 0, link.href.indexOf( suffix ) );
+      }
+    } else {
+      link.href = link.href + suffix;
+    }
+  }
+
+}
 function filter_sites() {
   var text = document.getElementById("search_box").value;
   var list = document.getElementById("drive_sites");
@@ -6,15 +23,15 @@ function filter_sites() {
   var extern_id = 0;
   for (var i = 0; i < items.length; ++i) {
     var item = items[i];
-    var item_text = item.textContent || item.innerText;
-    item_text = item_text.toLowerCase();
-    if (item_text.indexOf(text.toLowerCase()) > -1) {
+    var item_text = item.textcontent || item.innertext;
+    item_text = item_text.tolowercase();
+    if (item_text.indexof(text.tolowercase()) > -1) {
       item.style.display = "block";
       visible = true;
     } else {
       item.style.display = "none";
     }
-    if (item_text.indexOf('extern') > -1) {
+    if (item_text.indexof('extern') > -1) {
       extern_id = i;
     }
   }
