476a335355c811745967c83d8079cddd846aeca3
cartja <carina@sunet.se>
2024-06-04 12:02:29 +0200

    more info in footer, alternative design

--- static/drive.js
+++ static/drive.js
@@ -1,3 +1,4 @@
+// Direct login checkbox
 function use_direct() {
   var list = document.getElementById("drive_sites");
   var items = list.getElementsByTagName("li");
@@ -14,43 +15,8 @@ function use_direct() {
     }
   }
 }
-// function filter_sites() {
-//   var text = document.getElementById("search_box").value;
-//   var list = document.getElementById("drive_sites");
-//   var items = list.getElementsByTagName("li");
-//   var visible = false;
-//   var extern_id = 0;
-//   for (var i = 0; i < items.length; ++i) {
-//     let link = items[i].getElementsByTagName("a")[0];
-//     var item_text = link.innerText;
-//     item_text = item_text.toLowerCase();
-//     var shortname = link.dataset.shortname;
-//     var external_url = link.dataset.externalurl;
-//     var condition =
-//       //item_text.indexOf(text.toLowerCase()) > -1 ||
-//       shortname.indexOf(text.toLowerCase()) > -1 || external_url.indexOf(text.toLowerCase()) > -1;
-//     if (condition) {
-//       items[i].style.display = "inline";
-//       visible = true;
-//       list.style.display = "inline";
-//     } else {
-//       items[i].style.display = "none";
-//     }
-//     if (shortname.indexOf("extern") > -1) {
-//       extern_id = i;
-//     }
-//   }
-//   if (!visible) {
-//     document.getElementById(extern_id).style.display = "inline";
-//   }
-// }
 
-// function optionChanged(event) {
-//   item = event.target.value;
-//   console.log(item);
-//   document.getElementById("selectedLink").external_url = item.dataset.externalurl;
-// }
-
+// Filtering site-buttons using datalist/input
 function optionChanged(event) {
   var text = event.target.value;
   console.log(text);
@@ -81,4 +47,10 @@ function optionChanged(event) {
   if (!visible) {
     document.getElementById(extern_id).style.display = "inline";
   }
+}
+
+// Toggling info links in footer
+function showMore() {
+  document.getElementById("show_more").classList.toggle("shows");
+  return false;
 }
