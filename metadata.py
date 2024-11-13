cfd8c3df055562dc8d07e9d3e5eff8b86b476d56
Micke Nordin <kano@sunet.se>
2024-11-13 17:17:38 +0100

    Add config generation

---
+++ metadata.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+import requests
+import sys
+import yaml
+
+
+class MyDumper(yaml.Dumper):
+
+    def increase_indent(self, flow=False, indentless=False):
+        return super(MyDumper, self).increase_indent(flow, False)
+
+
+drive_scopes = ["antagning.se",
+                "bth.se",
+                "chalmers.se",
+                "du.se",
+                "esh.se",
+                "fhs.se",
+                "gih.se",
+                "gu.se",
+                "hb.se",
+                "hh.se",
+                "hhs.se",
+                "hig.se",
+                "his.se",
+                "hj.se",
+                "hkr.se",
+                "hv.se",
+                "irf.se",
+                "kau.se",
+                "kb.se",
+                "ki.se",
+                "kkh.se",
+                "kmh.se",
+                "konstfack.se",
+                "kth.se",
+                "kva.se",
+                "liu.se",
+                "lnu.se",
+                "ltu.se",
+                "lu.se",
+                "mau.se",
+                "mdu.se",
+                "miun.se",
+                "nordunet.se",
+                "nrm.se",
+                "oru.se",
+                "rkh.se",
+                "scilifelab.se",
+                "shh.se",
+                "sics.se",
+                "slu.se",
+                "smhi.se",
+                "sp.se",
+                "su.se",
+                "sunet.se",
+                "suni.se",
+                "swamid.se",
+                "ths.se",
+                "uhr.se",
+                "umu.se",
+                "uniarts.se",
+                "uu.se",
+                "vinnova.se",
+                "vr.se"]
+
+
+def validate(idps):
+    seen = []
+    problem = False
+    for key in idps:
+        idp = idps[key]
+        for entityID in idp['entityIDs']:
+            if entityID not in seen:
+                seen.append(entityID)
+            else:
+                problem = True
+                print(f"Duplicate entityID: {entityID}")
+    if problem:
+        sys.exit(1)
+
+
+def output(idps):
+    transformed = {"domain": "drive.test.sunet.se", "idps": []}
+
+    for key in idps:
+        idp = idps[key]
+        transformed["idps"].append(
+            {"id": key, "name": idp['name'], "entityIDs": idp['entityIDs']})
+    yaml.dump(transformed, sys.stdout, Dumper=MyDumper, sort_keys=False)
+
+
+def main():
+    idps = {}
+    # idps["extern"] = {"name": "Extern", 'entityIDs': []}
+    r = requests.get("https://mds.swamid.se/md/swamid-transitive-ds.json")
+    metadata = r.json()
+    for entity in metadata:
+        if 'scope' in entity and 'type' in entity and entity['type'] == 'idp':
+            scopes = entity['scope'].split(',')
+            for scope in scopes:
+                if scope in drive_scopes:
+                    index = scope.replace('.se', "")
+                    if index not in idps:
+                        idps[index] = {
+                            "name": entity['title'], 'entityIDs': []}
+                    elif (index in idps
+                          and (
+                              "test" in idps[index]['name'].lower()
+                              or "deprecated" in idps[index]['name'].lower()
+                          )
+                          and (
+                              "test" not in entity['title'].lower()
+                              or "deprecated" not in entity['title'].lower())
+                          ):
+                        idps[index]['name'] = entity['title']
+                    idps[index]['entityIDs'].append(entity['entityID'])
+    #    elif 'type' in entity and entity['type'] == 'idp':
+    #        idps['extern']['entityIDs'].append(entity['entityID'])
+    validate(idps)
+    output(idps)
+
+
+if __name__ == '__main__':
+    main()
