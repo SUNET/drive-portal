65a4a6c104b7c9dc12c7d2607d97830f4dfecd29
Micke Nordin <kano@sunet.se>
2024-12-09 11:54:41 +0100

    Move sp/sics/rise to extern

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
-from flask import (
-    Flask,
-    request,
-    send_from_directory,
-    redirect
-)
 import os
+
 import yaml
+from flask import Flask, redirect, request, send_from_directory
 
 app = Flask(__name__)
 
 # Load config
 yml = {}
-domain = os.environ.get('DRIVE_DOMAIN')
+domain = os.environ.get("DRIVE_DOMAIN")
 if domain is None:
     raise Exception("DRIVE_DOMAIN environment variable not set")
 if not os.path.exists("/app/config.yaml"):
     raise Exception("/app/config.yaml file not found")
-with open("/app/config.yaml", 'r') as fh:
+with open("/app/config.yaml", "r") as fh:
     yml = yaml.safe_load(fh)
 
 
 def map_entity(entityID):
-    for idp in yml['idps']:
-        if entityID in idp['entityIDs']:
-            mid = idp['id']
+    for idp in yml["idps"]:
+        if entityID in idp["entityIDs"]:
+            mid = idp["id"]
             # Map customers with different scope than name
-            if mid == 'mdh':
+            if mid == "mdh":
                 mid = "mdu"
-            if mid == 'nordu':
-                mid = 'nordunet'
-            if mid == 'ri':
-                mid = 'sics'
+            if mid == "nordu":
+                mid = "nordunet"
 
             drive = f"https://{mid}.{domain}/index.php/apps/user_saml"
             return f"{drive}/saml/login?idp_hint={entityID}"
@@ -38,23 +32,25 @@ def map_entity(entityID):
     return f"{drive}/saml/login?idp_hint={entityID}"
 
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico',
-                               mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     disco_url = "https://service.seamlessaccess.org/ds/"
-    if domain == 'drive.test.sunet.se':
+    if domain == "drive.test.sunet.se":
         disco_url += f"?entityID=https%3A%2F%2Fidp-proxy.{domain}%2Fsp&return="
     else:
-        disco_url += f"?entityID=https%3A%2F%2Fdrive-idp-proxy.sunet.se%2Fsp&return="
+        disco_url += "?entityID=https%3A%2F%2Fdrive-idp-proxy.sunet.se%2Fsp&return="
     disco_url += f"https%3A%2F%2F{domain}"
 
-    entityID = request.args.get('entityID')
+    entityID = request.args.get("entityID")
     if not entityID:
         return redirect(disco_url)
     else:
